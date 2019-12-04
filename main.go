3d261774af3cee1af14ce0b05827be3777433e29
Kai Hendry <hendry@iki.fi>
2019-12-04 13:47:06 +0800

    Manual registration

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	_ "net/http/pprof"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -21,29 +22,45 @@ var (
 	BuildUser string
 	BuildHost string
 	GoVersion = runtime.Version()
-)
 
-// Map provides the iterable version information.
-var Map = map[string]string{
-	"version":   Version,
-	"branch":    Branch,
-	"buildUser": BuildUser,
-	"goVersion": GoVersion,
-}
+	inFlightGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "in_flight_requests",
+		Help: "A guage of requests currently being served by the wrapped handler",
+	})
 
-func init() {
-	buildInfo := prometheus.NewGaugeVec(
+	counter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "requests_total",
+		Help: "A counter for requests to the wrapped handler",
+	},
+		[]string{"code", "method"})
+
+	duration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "request_duration_seconds",
+			Help: "A histogram of latencies for requests.",
+			// 50ms, 100ms, 200ms, 300ms, 500ms
+			Buckets: []float64{.05, .1, .2, .3, .5},
+		},
+		[]string{"handler", "code", "method"},
+	)
+
+	buildInfo = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "sla_build_info",
 			Help: "A metric with a constant '1' value labeled by attributes from which sla was built.",
 		},
 		[]string{"version", "branch", "buildUser", "goversion"},
 	)
-	buildInfo.WithLabelValues(Version, Branch, BuildUser, GoVersion).Set(1)
+)
+
+func init() {
+	prometheus.MustRegister(inFlightGauge)
+	prometheus.MustRegister(counter)
+	prometheus.MustRegister(duration)
 	prometheus.MustRegister(buildInfo)
+	buildInfo.WithLabelValues(Version, Branch, BuildUser, GoVersion).Set(1)
 }
 
-// Create the handlers that will be wrapped by the middleware.
 func root(w http.ResponseWriter, r *http.Request) {
 	sleep, err := strconv.Atoi(r.URL.Query().Get("sleep"))
 	if err == nil {
@@ -65,22 +82,11 @@ func main() {
 	// duration is partitioned by the HTTP method and handler. It uses custom
 	// buckets based on the expected request duration.
 	// https://prometheus.io/docs/practices/histograms/
-	// sum(rate(http_request_duration_seconds_bucket{le="0.3"}[5m])) by (job)
+	// sum(rate(request_duration_seconds_bucket{le="0.3"}[5m])) by (job)
 	// /
-	// sum(rate(http_request_duration_seconds_count[5m])) by (job)
-
-	duration := prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name: "http_request_duration_seconds",
-			Help: "A histogram of latencies for requests.",
-			// 50ms, 100ms, 200ms, 300ms, 500ms
-			Buckets: []float64{.05, .1, .2, .3, .5},
-		},
-		[]string{"handler", "method"},
-	)
+	// sum(rate(request_duration_seconds_count[5m])) by (job)
 
 	// Register all of the metrics in the standard registry.
-	prometheus.MustRegister(duration)
 
 	// Pprof server.
 	// https://mmcloughlin.com/posts/your-pprof-is-showing
@@ -91,9 +97,14 @@ func main() {
 	// Application server.
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
-	// Injecting the "handler" label by currying.
-	mux.Handle("/", promhttp.InstrumentHandlerDuration(duration.MustCurryWith(prometheus.Labels{"handler": "/"}),
-		http.HandlerFunc(root)))
+
+	rootChain := promhttp.InstrumentHandlerInFlight(
+		inFlightGauge,
+		promhttp.InstrumentHandlerDuration(
+			duration.MustCurryWith(prometheus.Labels{"handler": "root"}),
+			promhttp.InstrumentHandlerCounter(counter, http.HandlerFunc(root))))
+
+	mux.Handle("/", rootChain)
 
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), mux); err != nil {
 		log.Fatal(err)
